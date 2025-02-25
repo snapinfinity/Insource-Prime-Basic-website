3a3abde8f0395d36db3e5169ebea91a797249c23
NANDANALAKSHMI <nandanalakshmit@gmil.com>
2025-02-26 00:03:53 +0530

    ui

--- src/components/aboutus/OurVision.jsx
+++ src/components/aboutus/OurVision.jsx
@@ -16,7 +16,7 @@ const GetStarted = () => {
         >
             <div className="container px-10 mx-auto ">
                 <h2 className="mb-4 text-4xl font-bold text-left md:text-5xl md:text-center ">Our Vision</h2>
-                <p className="xl:w-[50%] w-full mx-auto mb-8 text-lg text-left md:text-center ">
+                <p className="xl:w-[50%] md:w-[70%] w-full mx-auto mb-8 text-lg text-left md:text-center ">
                     Our vision is to be the <span className="font-semibold">leading financial consultancy</span> that empowers businesses and individuals with <span className="font-semibold">seamless access</span> to UAE banking services.  
                     We are committed to <span className="font-semibold">efficiency, transparency, and professionalism</span>, ensuring our clients navigate financial landscapes with ease and confidence.  
                     By providing innovative solutions and expert guidance, we strive to open doors to <span className="font-semibold">new financial possibilities</span> in the UAE.
